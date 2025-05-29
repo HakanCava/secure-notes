9849c7e83b4dc3f22924e281451a1c1fb1118e71
HakanCava <hakancavaa@gmail.com>
2025-05-29 22:23:11 +0300

    watchmanconfig added

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import { Platform } from "react-native";
 
 import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
@@ -12,22 +13,27 @@ export default function TabLayout() {
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
-        // tabBarStyle: {
-        //   backgroundColor: "#112240",
-        //   borderTopWidth: 1,
-        //   borderTopColor: "#1E3A8A",
-        //   paddingBottom: Platform.OS === "ios" ? 20 : 10,
-        //   paddingTop: 10,
-        //   height: Platform.OS === "ios" ? 85 : 65,
-        // },
-        // tabBarActiveTintColor: "#64FFDA",
-        // tabBarInactiveTintColor: "#8892B0",
+        tabBarStyle: {
+          backgroundColor: "#112240",
+          borderTopWidth: 1,
+          borderTopColor: "#112240",
+          paddingBottom: Platform.OS === "ios" ? 20 : 10,
+          paddingTop: 10,
+          height: Platform.OS === "ios" ? 85 : 65,
+        },
+        tabBarActiveTintColor: "#64FFDA",
+        tabBarInactiveTintColor: "#8892B0",
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontFamily: "SpaceMono",
+          paddingBottom: Platform.OS === "ios" ? 0 : 4,
+        },
       }}
     >
       <Tabs.Screen
         name="home"
         options={{
-          title: "Home",
+          title: "Ana Sayfa",
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="house.fill" color={color} />
           ),
@@ -36,7 +42,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="settings"
         options={{
-          title: "Settings",
+          title: "Ayarlar",
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="gearshape.fill" color={color} />
           ),
--- components/ui/TabBarBackground.ios.tsx
+++ components/ui/TabBarBackground.ios.tsx
@@ -1,17 +1,8 @@
-import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
-import { BlurView } from 'expo-blur';
-import { StyleSheet } from 'react-native';
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import { StyleSheet, View } from "react-native";
 
 export default function BlurTabBarBackground() {
-  return (
-    <BlurView
-      // System chrome material automatically adapts to the system's theme
-      // and matches the native tab bar appearance on iOS.
-      tint="systemChromeMaterial"
-      intensity={100}
-      style={StyleSheet.absoluteFill}
-    />
-  );
+  return <View style={StyleSheet.absoluteFill} />;
 }
 
 export function useBottomTabOverflow() {
